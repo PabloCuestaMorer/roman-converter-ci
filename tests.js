c8544321f5f207ba0e5cb4c9e60c1e5ee03a254a
Bl0star <pablocuestam@outlook.com>
2023-05-09 19:09:08 +0200

    Update tests.js
    import the functions from romans.js and use them in the tests

--- tests.js
+++ tests.js
@@ -1,3 +1,6 @@
+// import the functions from romans.js and use them in the tests
+const { convertRomanToInteger, convertIntegerToRoman } = require("./romans");
+
 test("Example", function(assert) {
   assert.propEqual(convertRomanToInteger("I"), {value: 1, message: '', result: true}, "TC-1");
 });
